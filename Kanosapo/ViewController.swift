2e1cc725e6d9197182b417406d668ed18addd125
mckee0919 <s.mckee0919@gmail.com>
2020-11-12 15:26:02 +0900

    Initial Commit

---
+++ Kanosapo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kanosapo
+//
+//  Created by 牧内秀介 on 2020/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
